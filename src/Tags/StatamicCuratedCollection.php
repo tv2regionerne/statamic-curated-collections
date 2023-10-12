6b103ffca9ca0e8396ae3fa7915f20e86c0191f9
Sylvester Damgaard <sn@cbox.dk>
2023-10-12 13:42:55 +0200

    Fallback should only return published entries

--- src/Tags/StatamicCuratedCollection.php
+++ src/Tags/StatamicCuratedCollection.php
@@ -59,6 +59,7 @@ class StatamicCuratedCollection extends Tags
         if ($fallback && count($entries) < $limit) {
             $fallbackEntries = Entry::query()
                 ->where('collection', $curatedCollection->fallback_collection)
+                ->where('status', 'published')
                 ->whereNotIn('id', $ids)
                 ->limit($limit - count($entries))
                 ->orderBy($curatedCollection->fallback_sort_field, $curatedCollection->fallback_sort_direction)
