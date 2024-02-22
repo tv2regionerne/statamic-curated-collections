1d6c2f20a7951508a9603bd05cd37410ff6c326f
Ryan Mitchell <ryan@thoughtcollective.com>
2024-02-22 09:17:16 +0000

    Simplify routes/cp.php

--- routes/cp.php
+++ routes/cp.php
@@ -6,16 +6,35 @@ use Tv2regionerne\StatamicCuratedCollection\Http\Controllers\CP\ApiEntryRelation
 use Tv2regionerne\StatamicCuratedCollection\Http\Controllers\CP\CuratedCollectionBlueprintController;
 use Tv2regionerne\StatamicCuratedCollection\Http\Controllers\CP\CuratedCollectionController;
 
-Route::get('/curated-collections/api/collections/{curatedCollection:handle}/entries', [ApiEntriesController::class, 'index'])->name('curated-collections.api.entries.index');
-Route::post('/curated-collections/api/collections/{curatedCollection:handle}/entries', [ApiEntriesController::class, 'store'])->name('curated-collections.api.entries.store');
-Route::post('/curated-collections/api/collections/{curatedCollection:handle}/entries/reorder', [ApiEntriesController::class, 'reorder'])->name('curated-collections.api.entries.reorder');
-Route::get('/curated-collections/api/collections/{curatedCollection:handle}/entries/create', [ApiEntriesController::class, 'create'])->name('curated-collections.api.entries.create');
-Route::get('/curated-collections/api/collections/{curatedCollection:handle}/entries/{curatedCollectionEntry}', [ApiEntriesController::class, 'edit'])->name('curated-collections.api.entries.edit');
-Route::patch('/curated-collections/api/collections/{curatedCollection:handle}/entries/{curatedCollectionEntry}', [ApiEntriesController::class, 'update'])->name('curated-collections.api.entries.update');
-Route::delete('/curated-collections/api/collections/{curatedCollection:handle}/entries/{curatedCollectionEntry}', [ApiEntriesController::class, 'destroy'])->name('curated-collections.api.entries.destroy');
+Route::resource('/curated-collections', CuratedCollectionController::class);
 
-Route::get('/curated-collections/api/entry-relations/{id}', [ApiEntryRelationController::class, 'index'])->name('curated-collections.api.entry-relation');
+Route::prefix('/curated-collections')
+    ->group(function () {
 
-Route::resource('curated-collections', CuratedCollectionController::class);
-Route::get('/curated-collections/{curatedCollection}/blueprint', [CuratedCollectionBlueprintController::class, 'edit'])->name('curated-collections.blueprint.edit');
-Route::patch('/curated-collections/{curatedCollection}/blueprint', [CuratedCollectionBlueprintController::class, 'update'])->name('curated-collections.blueprint.update');
+        Route::prefix('/{curatedCollection}/blueprint')
+            ->name('curated-collections.blueprint.')
+            ->group(function () {
+                Route::get('/', [CuratedCollectionBlueprintController::class, 'edit'])->name('edit');
+                Route::patch('/', [CuratedCollectionBlueprintController::class, 'update'])->name('update');
+            });
+
+        Route::prefix('/api')
+            ->name('curated-collections.api.')
+            ->group(function () {
+
+                Route::prefix('/collections/{curatedCollection:handle}/entries')
+                    ->name('entries.')
+                    ->group(function () {
+                        Route::get('/', [ApiEntriesController::class, 'index'])->name('entries.index');
+                        Route::post('/', [ApiEntriesController::class, 'store'])->name('entries.store');
+                        Route::post('/reorder', [ApiEntriesController::class, 'reorder'])->name('entries.reorder');
+                        Route::get('/create', [ApiEntriesController::class, 'create'])->name('entries.create');
+                        Route::get('/{curatedCollectionEntry}', [ApiEntriesController::class, 'edit'])->name('entries.edit');
+                        Route::patch('/{curatedCollectionEntry}', [ApiEntriesController::class, 'update'])->name('entries.update');
+                        Route::delete('/{curatedCollectionEntry}', [ApiEntriesController::class, 'destroy'])->name('entries.destroy');
+                    });
+
+                Route::get('/entry-relations/{id}', [ApiEntryRelationController::class, 'index'])->name('entry-relation');
+
+            });
+    });
